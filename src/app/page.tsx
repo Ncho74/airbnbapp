7257fbb1094f99f46a87b2b7c5dec70965c1c122
ncho23 <isaacncho9@gmail.com>
2023-06-26 17:53:31 +0000

    Airbnb modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from 'next/image'
+'use client';
 import React from 'react'
 import Navbar from '@/components/navbar/navbar'
+import ClientOnly from '@/components/ClientOnly'
+import Modal from '@/components/modals/Modal'
 
 export default function Home() {
   return (
     <>
-    <Navbar ></Navbar >
- <div className='text-rose-500 text-2xl'>Hello Airbnb</div>
+    <ClientOnly >
+    <Modal isOpen={true} onClose={()=>{}} onSubmit={()=>{} } actionLabel={''} title='Model'/>
+    <Navbar />
+    </ClientOnly>
+ 
 
     </>
   )
